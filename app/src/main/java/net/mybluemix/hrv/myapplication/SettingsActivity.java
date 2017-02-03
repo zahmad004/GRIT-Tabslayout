3d19be28857156e1b91b831bc92833c3d9fe1f81
zahmad004 <z@zahmad.com>
2017-02-03 09:43:48 -0500

    intial file dump

---
+++ app/src/main/java/net/mybluemix/hrv/myapplication/SettingsActivity.java
@@ -0,0 +1,13 @@
+package net.mybluemix.hrv.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
